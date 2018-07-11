5529794c8ae862b5661b4147259d1d8b05588b8c
santiago <sabeloqui@gmail.com>
2018-07-11 13:07:36 -0300

    Arreglo animaciones

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 //Animaciones
+//-----------
+
+
 let article4P = document.getElementById("article-4-p");
 let article6P = document.getElementById("article-6-p");
 let article8P = document.getElementById("article-8-p");
@@ -13,31 +16,40 @@ let contactElement1 = document.getElementById("contact-element1");
 let contactElement2 = document.getElementById("contact-element2");
 let contactElement3 = document.getElementById("contact-element3");
 
+//Localización de los elementos
+let coordsArticle4P = article4P.getBoundingClientRect().top;
+let coordsArticle6P = article6P.getBoundingClientRect().top;
+let coordsArtistH2 = artistH2.getBoundingClientRect().top;
+let coordsCatalogueH2 = catalogueH2.getBoundingClientRect().top;
+let coordsContactElement = contactElement1.getBoundingClientRect().top;
+
+//Función de animación
+
 window.onscroll = function() {article6And8Animation();};
 
 function article6And8Animation() {
 
-    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
         article2H2.className = "article-h2-animation";
         article2P.className = "article-p-animation";
     } 
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    if (document.body.scrollTop > coordsArticle4P - (coordsArticle4P * 0.40) || document.documentElement.scrollTop > coordsArticle4P - (coordsArticle4P * 0.40)) {
         article4P.className = "article-4-animation";
     } 
-    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    if (document.body.scrollTop > coordsArticle6P - (coordsArticle6P * 0.40) || document.documentElement.scrollTop > coordsArticle6P - (coordsArticle6P * 0.40)) {
         article6P.className = "article-6-animation";
         article8P.className = "article-8-animation";
     } 
-    if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    if (document.body.scrollTop > coordsArtistH2 - (coordsArticle6P * 0.40) || document.documentElement.scrollTop > coordsArtistH2 - (coordsArticle6P * 0.40)) {
         artistH2.className = "artist-h2-animation";
         artistP.className = "artist-p-animation";
     } 
-    if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+    if (document.body.scrollTop > coordsArtistH2 - (coordsCatalogueH2 * 0.10) || document.documentElement.scrollTop > coordsArtistH2 - (coordsCatalogueH2 * 0.10)) {
         catalogueH2.className = "catalogue-h2-animation";
         catalogueP.className = "catalogue-p-animation";
         catalogueButton.className = "catalogue-button-animation";
     }  
-    if (document.body.scrollTop > 1800 || document.documentElement.scrollTop > 1800){
+    if (document.body.scrollTop > coordsContactElement - (coordsContactElement * 0.20) || document.documentElement.scrollTop > coordsContactElement - (coordsContactElement * 0.20)){
         contactElement1.className = "contact-element-1-animation";
         contactElement2.className = "contact-element-2-animation";
         contactElement3.className = "contact-element-3-animation";
