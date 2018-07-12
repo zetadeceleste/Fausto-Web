c9f09ff096d2b0142fcc4df34b286e54724643f7
Celeste <zdceleste@gmail.com>
2018-07-12 13:03:53 -0300

    Cambios

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 //Animaciones
 //-----------
 
-
 let article4P = document.getElementById("article-4-p");
 let article6P = document.getElementById("article-6-p");
 let article8P = document.getElementById("article-8-p");
@@ -35,63 +34,61 @@ let coordsParkSection = parkSection.getBoundingClientRect().top;
 let coordsCatalogueSection = catalogueSection.getBoundingClientRect().top;
 let coordsContactSection = contactSection.getBoundingClientRect().top;
 
-
 //Smooth scroll
-navElement1.addEventListener("click", function(){
-    window.scroll( {
-        top: coordsArtistSection - 10, 
-        left: 0, 
-        behavior: 'smooth' 
+navElement1.addEventListener("click", function () {
+    window.scroll({
+        top: coordsArtistSection - 10,
+        left: 0,
+        behavior: 'smooth'
     });
 });
 
-navElement2.addEventListener("click", function(){
-    window.scroll( {
-        top: coordsParkSection - 10, 
-        left: 0, 
-        behavior: 'smooth' 
+navElement2.addEventListener("click", function () {
+    window.scroll({
+        top: coordsParkSection - 10,
+        left: 0,
+        behavior: 'smooth'
     });
 });
 
-navElement3.addEventListener("click", function(){
-    window.scroll( {
-        top: coordsCatalogueSection - 10, 
-        left: 0, 
-        behavior: 'smooth' 
+navElement3.addEventListener("click", function () {
+    window.scroll({
+        top: coordsCatalogueSection - 10,
+        left: 0,
+        behavior: 'smooth'
     });
 });
 
-navElement4.addEventListener("click", function(){
-    window.scroll( {
-        top: coordsContactSection - 10, 
-        left: 0, 
-        behavior: 'smooth' 
+navElement4.addEventListener("click", function () {
+    window.scroll({
+        top: coordsContactSection - 10,
+        left: 0,
+        behavior: 'smooth'
     });
 });
-
 
 //Función de animación
 
-window.onscroll = function() {article6And8Animation();};
+window.onscroll = function () { article6And8Animation(); };
 
 function article6And8Animation() {
 
     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
         article2H2.className = "article-h2-animation";
         article2P.className = "article-p-animation";
-    } 
+    }
     if (document.body.scrollTop > coordsArticle4P - (coordsArticle4P * 0.40) || document.documentElement.scrollTop > coordsArticle4P - (coordsArticle4P * 0.40)) {
         article4P.className = "article-4-animation";
-    } 
+    }
     if (document.body.scrollTop > coordsArticle6P - (coordsArticle6P * 0.40) || document.documentElement.scrollTop > coordsArticle6P - (coordsArticle6P * 0.40)) {
         article6P.className = "article-6-animation";
         article8P.className = "article-8-animation";
-    } 
+    }
     if (document.body.scrollTop > coordsParkH2 - (coordsParkH2 * 0.30) || document.documentElement.scrollTop > coordsParkH2 - (coordsParkH2 * 0.30)) {
         parkH2.className = "park-h2-animation";
         parkP.className = "park-p-animation";
-    }  
-    if (document.body.scrollTop > coordsContactElement - (coordsContactElement * 0.20) || document.documentElement.scrollTop > coordsContactElement - (coordsContactElement * 0.20)){
+    }
+    if (document.body.scrollTop > coordsContactElement - (coordsContactElement * 0.20) || document.documentElement.scrollTop > coordsContactElement - (coordsContactElement * 0.20)) {
         contactElement1.className = "contact-element-1-animation";
         contactElement2.className = "contact-element-2-animation";
         contactElement3.className = "contact-element-3-animation";
